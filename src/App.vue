632df7765d2421f282c3a879ea4a1b9d96100c45
Undostresdoce <camictejo@gmail.com>
2025-09-04 21:09:50 -0400

    se añade un footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <Formulario />
+  <div class="d-flex flex-column min-vh-100">
+    <main class="flex-grow-1">
+      <Formulario />
+    </main>
+
+    <footer class="bg-dark text-white text-center py-3">
+      <small>&copy; Camila Collado - Daniel Paredes - Jesús Pérez</small>
+    </footer>
+  </div>
 </template>
 
 <script>
